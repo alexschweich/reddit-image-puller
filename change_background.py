050b09e5d7e09014e4f529d42ff8be817690b647
alexschweich <alex.schweich95@gmail.com>
2019-07-07 22:47:22 -0400

    Use current working directory

--- change_background.py
+++ change_background.py
@@ -6,7 +6,6 @@ import random
 
 def main():
     filepath = os.getcwd() + '/images/' + random.choice(os.listdir('images'))
-    print(filepath)
     if os.name == 'nt':  # Windows
         SPI_SETDESKWALLPAPER = 20
         ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, filepath, 0)
